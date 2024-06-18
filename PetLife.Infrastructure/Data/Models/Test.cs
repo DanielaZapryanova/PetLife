d6287ad792badd820924f1841253bd266b4c93fc
DanielaZapryanova <140154760+DanielaZapryanova@users.noreply.github.com>
2024-06-18 23:30:33 +0300

    Added Test entity.

---
+++ PetLife.Infrastructure/Data/Models/Test.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetLife.Infrastructure.Data.Models
+{
+    /// <summary>
+    /// Tests.
+    /// </summary>
+    [Comment("Tests")]
+    public class Test
+    {
+        [Key]
+        [Comment("Id of the test.")]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime DateOfConduction { get; set; }
+
+        public string Path { get; set; }
+
+        //public Pet Pet { get; set; }
+    }
+}
