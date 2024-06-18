81c53c1469708b2f044f2090b0d7ef4df80a5abc
DanielaZapryanova <140154760+DanielaZapryanova@users.noreply.github.com>
2024-06-18 23:31:39 +0300

    Initial set-up of database.

---
+++ PetLife.Infrastructure/Data/PetLifeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetLife.Infrastructure.Data.Models;
+
+namespace PetLife.Infrastructure.Data
+{
+    public class PetLifeDbContext : DbContext
+    {
+        public PetLifeDbContext()
+        {
+        }
+
+        public PetLifeDbContext(DbContextOptions<PetLifeDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Test> Pets { get; set; }
+    }
+}
