2b9b1fd32e6a000102067bf633c0493b722127b2
DanielaZapryanova <140154760+DanielaZapryanova@users.noreply.github.com>
2024-06-20 12:45:41 +0300

    Created Treatments Table .

---
+++ PetLife.Infrastructure/Data/Models/Treatment.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetLife.Infrastructure.Data.Models
+{
+    /// <summary>
+    /// Treatments.
+    /// </summary>
+    [Comment("Treatments")]
+    public class Treatment
+    {
+        [Key]
+        [Comment("Id of the treatments.")]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{yyyy-MM-dd}")]
+        public DateTime DateOfTreatment { get; set; }
+    }
+}
--- PetLife.Infrastructure/Data/PetLifeDbContext.cs
+++ PetLife.Infrastructure/Data/PetLifeDbContext.cs
@@ -14,5 +14,6 @@ namespace PetLife.Infrastructure.Data
         }
 
         public DbSet<Test> Pets { get; set; }
+        public DbSet<Treatment> Treatments { get; set; }
     }
 }
