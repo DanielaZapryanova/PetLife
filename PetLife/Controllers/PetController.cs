3a88a3b86294db690d002b661d012c1a89cc4a89
DanielaZapryanova <140154760+DanielaZapryanova@users.noreply.github.com>
2024-07-02 16:45:37 +0300

    Create PetController.

---
+++ PetLife/Controllers/PetController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PetLife.Contracts;
+
+namespace PetLife.Controllers
+{
+    public class PetController : Controller
+    {
+        IPetService petService;
+        IOwnerService ownerService;
+
+        public PetController(IPetService petService, IOwnerService ownerService)
+        {
+            this.petService = petService;
+            this.ownerService = ownerService;
+        }
+
+        [Authorize(Roles = "Admin,Vet")]
+        public async Task<IActionResult> AddPet()
+        {
+            AddPetViewModel addPetViewModel = new AddPetViewModel();
+            IList<OwnerViewModel> possibleOwners = new List<OwnerViewModel>();
+            possibleOwners = await ownerService.GetAllOwners();
+            addPetViewModel.PossibleOwners = possibleOwners;
+            addPetViewModel.DateOfBirth = DateTime.Today;
+            return View(addPetViewModel);
+        }
+
+    }
+}
